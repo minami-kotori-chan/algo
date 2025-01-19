d6c8f39ca0b64e68cb2f3144757cdb4c426c1105
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-01-20 03:24:20 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 220 ms, Memory: 121912 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,20 +1,25 @@
 import sys
 
 prime=[True]*1000001
+prime_set=[]
 prime[1]=False
-k=int(1000000**0.5)+1
-for i in range(2,k):
-    if prime[i]==False:
-        continue
-    for j in range(i*i,1000001,i):
-        prime[j]=False
+
+for i in range(2,1000001):
+    if prime[i]==True:
+        prime_set.append(i)
+    for j in prime_set:
+        if i*j>1000000:
+            break
+        prime[i*j]=False
+        if i%j==0:
+            break
 while True:
     goldbach_check=False
     n=int(sys.stdin.readline().rstrip())
     if n==0:
         break
-    for i in range(2,n):
-        if prime[i]==True and prime[n-i]==True:
+    for i in prime_set:
+        if prime[n-i]==True:
             print(f"{n} = {i} + {n-i}")
             goldbach_check=True
             break
