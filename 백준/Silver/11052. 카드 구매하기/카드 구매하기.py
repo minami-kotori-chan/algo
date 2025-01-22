9105e12d6abd32273c72fee44d92040c9bb378ce
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-01-22 21:21:44 +0900

    [Silver I] Title: 카드 구매하기, Time: 300 ms, Memory: 32552 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -14,8 +14,9 @@ def dp(n):
         return value[n]
     if n==1:
         value[n]=p[n]
-    for i in range(1,n):
-        value[n]=max(dp(i)+dp(n-i),p[n],value[n])
+    for i in range(1,int(n/2)+1):
+        value[n]=max(dp(i)+dp(n-i),value[n])
+    value[n] = max(value[n],p[n])
     return value[n]
 
 print(dp(n))
