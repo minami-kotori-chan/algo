564ab76e35fc26ec46a6e7b4807f9d9ed13c1b28
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-02-01 22:34:05 +0900

    [Silver III] Title: N과 M (8), Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,32 @@
+import sys
+
+n,m=map(int,sys.stdin.readline().rstrip().split())
+
+a=list(map(int,sys.stdin.readline().rstrip().split()))
+a.sort()
+r=[]
+v=[0]*n
+
+def dfs():
+    if len(r)==m:
+        print(*r)
+        return
+    if len(r)==0:
+        for i in range(n):
+            if v[i]==1:
+                continue
+            v[i]=1
+            r.append(a[i])
+            dfs()
+            r.pop()
+            v[i]=0
+    else:
+        l=a.index(r[-1])
+        for i in range(l,n):
+            v[i]=1
+            r.append(a[i])
+            dfs()
+            r.pop()
+            v[i]=0
+
+dfs()
