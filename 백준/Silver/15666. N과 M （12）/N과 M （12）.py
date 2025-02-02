2d05abbe38937d22be87990ef94f9ea48cc398df
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-02-02 16:34:45 +0900

    [Silver II] Title: N과 M (12), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,24 @@
+import sys
+
+n,m=map(int,sys.stdin.readline().rstrip().split())
+
+numbers=list(map(int,sys.stdin.readline().rstrip().split()))
+numbers.sort()
+stack=[]
+v=[0]*(n+1)
+
+def dfs(index=0):
+    check=0
+    if len(stack)==m:
+        print(*stack)
+        return
+    for i in range(index,n):
+        if check==numbers[i]:
+            continue
+        v[i]=1
+        stack.append(numbers[i])
+        check=numbers[i]
+        dfs(i)
+        stack.pop()
+        v[i]=0
+dfs()
