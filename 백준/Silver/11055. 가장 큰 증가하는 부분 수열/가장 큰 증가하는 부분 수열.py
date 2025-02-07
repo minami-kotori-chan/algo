e3a0babf1536f24e000d236cb96335857331ad65
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-02-07 19:18:10 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -0,0 +1,18 @@
+import sys
+
+n=int(sys.stdin.readline().rstrip())
+numbers=list(map(int,sys.stdin.readline().rstrip().split()))
+v=[0]*n
+
+v[0]=numbers[0]
+
+for i in range(1,n):
+    max_num=0
+    for j in range(i):
+        if numbers[i]>numbers[j]:
+            max_num=max(max_num,v[j])
+    v[i]=max_num+numbers[i]
+result=0
+for i in range(n):
+    result=max(result,v[i])
+print(result)
