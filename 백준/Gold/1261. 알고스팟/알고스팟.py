25d49382e84aac458447c1e87d9aacdb5f716b2c
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-02-11 22:40:26 +0900

    [Gold IV] Title: 알고스팟, Time: 96 ms, Memory: 35164 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -16,13 +16,25 @@ visited=[[-1]*m for _ in range(n)]
 visited[0][0]=0
 dr=[-1,0,1,0]
 dc=[0,1,0,-1]
-while q:
-    cur_row,cur_col=q.popleft()
-    cost=visited[cur_row][cur_col]
-    for i in range(4):
-        if cur_row+dr[i]>=n or cur_row+dr[i]<0 or cur_col+dc[i]>=m or cur_col+dc[i]<0:
-            continue
-        if visited[cur_row+dr[i]][cur_col+dc[i]]==-1 or visited[cur_row+dr[i]][cur_col+dc[i]]>cost+graph[cur_row+dr[i]][cur_col+dc[i]]:
-            q.append([cur_row+dr[i],cur_col+dc[i]])
-            visited[cur_row+dr[i]][cur_col+dc[i]]=cost+graph[cur_row+dr[i]][cur_col+dc[i]]
-print(visited[n-1][m-1])
+is_end=False
+if n==1 and m==1:
+    print(0)
+else:
+    while q:
+        cur_row,cur_col=q.popleft()
+        cost=visited[cur_row][cur_col]
+        for i in range(4):
+            if cur_row+dr[i]>=n or cur_row+dr[i]<0 or cur_col+dc[i]>=m or cur_col+dc[i]<0:
+                continue
+            if visited[cur_row+dr[i]][cur_col+dc[i]]==-1 or visited[cur_row+dr[i]][cur_col+dc[i]]>cost+graph[cur_row+dr[i]][cur_col+dc[i]]:
+                if graph[cur_row+dr[i]][cur_col+dc[i]]==0:
+                    q.appendleft([cur_row+dr[i],cur_col+dc[i]])
+                else:
+                    q.append([cur_row+dr[i],cur_col+dc[i]])
+                visited[cur_row+dr[i]][cur_col+dc[i]]=cost+graph[cur_row+dr[i]][cur_col+dc[i]]
+            if cur_row+dr[i]==n-1 and cur_col+dc[i]==m-1:
+                print(cost+graph[cur_row+dr[i]][cur_col+dc[i]])
+                is_end=True
+                break
+        if is_end:
+            break
