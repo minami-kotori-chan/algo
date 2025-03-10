fb32ea739b5cfbb04451c51525925f81e98f9939
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-03-10 12:41:49 +0900

    [Silver II] Title: 민균이의 계략, Time: 168 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.py
@@ -0,0 +1,10 @@
+import sys
+
+n=int(sys.stdin.readline().rstrip())
+arr=list(map(int,sys.stdin.readline().rstrip().split()))
+v=[1]*n
+for i in range(1,n):
+    for j in range(i):
+        if arr[i]>arr[j]:
+            v[i]=max(v[i],v[j]+1)
+print(max(v))
