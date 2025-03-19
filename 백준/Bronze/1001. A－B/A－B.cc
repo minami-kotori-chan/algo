ad706788dd0e075ee58669b95a9c2958e28263c1
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-03-19 22:10:21 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+#define fast_io cin.tie(NULL);cout.tie(NULL);ios_base::sync_with_stdio(false);
+
+int main(void){
+    fast_io;
+    int a,b;
+    cin>>a>>b;
+    cout<<a-b;
+    
+    return 0;
+}
