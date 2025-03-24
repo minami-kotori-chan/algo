f0c06b9c62e590e7c8ec69bd64d01a649ae4bcd3
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-03-24 20:26:56 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define fast_io cin.tie(NULL);cout.tie(NULL);ios_base::sync_with_stdio(false);
+
+int main(void){
+    fast_io;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
