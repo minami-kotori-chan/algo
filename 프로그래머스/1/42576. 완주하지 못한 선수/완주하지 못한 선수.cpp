5ec8530d3438c601d63a2c8a692283034e7da660
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-09-10 11:59:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.10 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,23 +1,26 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
-    unordered_map<string,int> hash_map1;
-    unordered_map<string,int> hash_map2;
+    unordered_map<string,int> hash;
+    for(const auto& i : participant){
+        if(hash.find(i)==hash.end()){
+            hash[i]=1;
+        }
+        else{
+            hash[i]+=1;
+        }
+    }
     for(const auto& i : completion){
-        hash_map1[i];
-        hash_map1[i]+=1;
+        hash[i]--;
     }
-    for(const auto& i : participant){
-        hash_map2[i];
-        hash_map2[i]+=1;
-    }
-    for(const auto& i : participant){
-        if(hash_map1[i]!=hash_map2[i]){
-            answer=i;
+    for(const auto& i : hash){
+        if(hash[i.first]!=0){
+            answer=i.first;
         }
     }
     return answer;
