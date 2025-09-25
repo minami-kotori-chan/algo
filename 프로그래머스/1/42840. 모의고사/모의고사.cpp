0920be7e43040b4a199150bf4026ec0f2cc1c623
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-09-25 18:02:44 +0900

    [level 1] Title: 모의고사, Time: 0.07 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -5,24 +5,27 @@ using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    int an1[5]={1,2,3,4,5};
-    int an2[8]={2,1,2,3,2,4,2,5};
-    int an3[10]={3,3,1,1,2,2,4,4,5,5};
-    int score[3]={0,};
+    int st1[]={1,2,3,4,5};
+    int st2[]={2,1,2,3,2,4,2,5};
+    int st3[]={3,3,1,1,2,2,4,4,5,5};
+    int scores[4]={0,};
+    
     for(int i=0;i<answers.size();i++){
-        if(an1[i%5]==answers[i]){
-            score[0]++;
-        }
-        if(an2[i%8]==answers[i]){
-            score[1]++;
-        }
-        if(an3[i%10]==answers[i]){
-            score[2]++;
+        if(answers[i]==st1[i%5]) scores[1]++;
+        if(answers[i]==st2[i%8]) scores[2]++;
+        if(answers[i]==st3[i%10]) scores[3]++;
+    }
+    int mx=0;
+    for(int i=1;i<=3;i++){
+        if(scores[i]>mx){
+            mx=scores[i];
         }
     }
-    int v=max(score[0],max(score[1],score[2]));
-    for(int i=0;i<3;i++){
-        if(score[i]==v) answer.push_back(i+1);
+    for(int i=1;i<=3;i++){
+        if(scores[i]==mx){
+            answer.push_back(i);
+        }
     }
+    
     return answer;
 }
