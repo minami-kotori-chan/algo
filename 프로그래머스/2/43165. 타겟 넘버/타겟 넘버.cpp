6b463bd1631d793e2eac4df46a933236968afb8d
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-09-26 21:05:49 +0900

    [level 2] Title: 타겟 넘버, Time: 6.04 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -2,58 +2,22 @@
 #include <vector>
 
 using namespace std;
-vector<int> nums;
-vector<int> stack;
-vector<int> v;
-int t;
-int result=0;
-int sum_stack()
-{
-    int sum=0;
-    for(const auto& i : stack){
-        sum+=nums[i];
-    }
-    return sum;
-}
 
-void dfs(int size=0)
+int result=0;
+
+void dfs(vector<int>& numbers,const int& target,int sum,int n)
 {
-    if(size==0)
-    {
-        for(int i=1;i<=nums.size();i++){
-            dfs(i);
-        }
+    if(n==numbers.size()){
+        if(target==sum) result++;
+        return;
     }
-    else
-    {
-        if(stack.size()==size){
-            if(sum_stack()==t){
-                result++;
-            }
-            return;
-        }
-        for(int i=0;i<nums.size();i++){
-            if(v[i]==1) continue;
-            if(!stack.empty() && stack.back()>=i) continue;
-            v[i]=1;
-            stack.push_back(i);
-            dfs(size);
-            stack.pop_back();
-            v[i]=0;
-        }
-    }
+    dfs(numbers,target,sum+numbers[n],n+1);
+    dfs(numbers,target,sum-numbers[n],n+1);
 }
 
 int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    int sum = 0;
-    for(const auto& i : numbers){
-        nums.push_back(i);
-        v.push_back(0);
-        sum+=i;
-    }
-    t=(sum-target)/2;
-    dfs();
-    answer=result;
+    int answer=0;
+    dfs(numbers,target,0,0);
+    answer = result;
     return answer;
 }
