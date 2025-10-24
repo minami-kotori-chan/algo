c931c1fda04d6b6e3f4ce15348572345e94f452a
kotori <42664480+minami-kotori-chan@users.noreply.github.com>
2025-10-24 18:05:18 +0900

    [Silver V] Title: 카우버거, Time: 0 ms, Memory: 2936 KB -BaekjoonHub

---
+++ 백준/Silver/15720. 카우버거/카우버거.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define FAST_IO cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(false);
+
+int arr[200000] = { 0, };
+int main(void) {
+	FAST_IO;
+	int b, c, d;
+	cin >> b >> c >> d;
+	vector<int> buger(b);
+	vector<int> side(c);
+	vector<int> drink(d);
+	int sum = 0;
+	for (int i = 0; i < b; i++) {
+		cin >> buger[i];
+		sum += buger[i];
+	}
+	for (int i = 0; i < c; i++) {
+		cin >> side[i];
+		sum += side[i];
+	}
+	for (int i = 0; i < d; i++) {
+		cin >> drink[i];
+		sum += drink[i];
+	}
+	cout << sum<<"\n";
+	sort(buger.begin(), buger.end(),greater<int>());
+	sort(side.begin(), side.end(), greater<int>());
+	sort(drink.begin(), drink.end(), greater<int>());
+	int size_ = min({ b,c,d });
+	for (int i = 0; i < size_; i++) {
+		sum -= buger[i] / 10;
+		sum -= side[i] / 10;
+		sum -= drink[i] / 10;
+	}
+	cout << sum;
+	return 0;
+}
